022a162eb2b016e026b1c89f8976863788dc4a31
Tom Corbin <30200976+ThwCorbin@users.noreply.github.com>
2020-08-19 21:01:10 -0400

    Add build

---
+++ build/precache-manifest.d0a149e755d30fb08f1dae746ee3bf0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "778b0dd332c7529efc5911899b59ac03",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c7e94e19e0ffb1232bd7",
+    "url": "/static/css/main.195db981.chunk.css"
+  },
+  {
+    "revision": "2ceebf223ce5d9a2f6c9",
+    "url": "/static/js/2.5ddea551.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5ddea551.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7e94e19e0ffb1232bd7",
+    "url": "/static/js/main.fddbe445.chunk.js"
+  },
+  {
+    "revision": "af974f994a9fe687f591",
+    "url": "/static/js/runtime-main.f0b01f21.js"
+  }
+]);
